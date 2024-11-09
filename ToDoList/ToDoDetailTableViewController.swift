70816e6a3fccb5f2d613fdc077bedc4a80ed89e1
marif.manafov <marif.manafov@aijus.com>
2024-11-09 11:14:01 +0900

    date picker を隠せるように修正

--- ToDoList/ToDoDetailTableViewController.swift
+++ ToDoList/ToDoDetailTableViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class ToDoDetailTableViewController: UITableViewController {
+    var isDatePickerHidden = true
+    let dateLabelIndexPath = IndexPath(row: 0, section: 1)
+    let datePickerIndexPath = IndexPath(row: 1, section: 1)
+    let notesIndexPath = IndexPath(row: 0, section: 2)
     
     @IBOutlet weak var titleTextField: UITextField!
     
@@ -23,6 +27,8 @@ class ToDoDetailTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        dueDatePicker.date = Date().addingTimeInterval(24*60*60)
+        updateDueDateLabel(date: dueDatePicker.date)
         updateSaveButtonState()
     }
     
@@ -31,7 +37,54 @@ class ToDoDetailTableViewController: UITableViewController {
         saveButton.isEnabled = shouldEnableSaveButton
     }
     
+    func updateDueDateLabel(date: Date) {
+        dueDateLabel.text = date.formatted(.dateTime.month(.defaultDigits).day().year(.twoDigits).hour().minute())
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case datePickerIndexPath where isDatePickerHidden:
+            return 0
+        case notesIndexPath:
+            return 200
+        default:
+            return UITableView.automaticDimension
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case datePickerIndexPath:
+            return 217
+        case notesIndexPath:
+            return 200
+        default: return UITableView.automaticDimension
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath == dateLabelIndexPath {
+            isDatePickerHidden.toggle()
+            updateDueDateLabel(date: dueDatePicker.date)
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        }
+    }
+    
     @IBAction func textEditingChanged(_ sender: UITextField) {
         updateSaveButtonState()
+    }
+    
+    @IBAction func returnPressed(_ sender: UITextField) {
+        sender.resignFirstResponder()
+    }
+    
+    @IBAction func isCompleteButtonTapped(_ sender: UIButton) {
+        isCompleteButton.isSelected.toggle()
+    }
+    
+    @IBAction func datePickerChanged(_ sender: UIDatePicker) {
+        updateDueDateLabel(date: sender.date)
     }
 }
