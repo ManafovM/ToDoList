83b692a59f68e4c45c28e529484f552592196855
marif.manafov <marif.manafov@aijus.com>
2024-11-11 18:06:04 +0900

    to-do を削除できないバグの修正

--- ToDoList/ToDoTableViewController.swift
+++ ToDoList/ToDoTableViewController.swift
@@ -17,18 +17,14 @@ class ToDoTableViewController: UITableViewController, UISearchResultsUpdating, T
     let searchController = UISearchController()
     var dataSource: UITableViewDiffableDataSource<Section, ToDo.ID>!
     
+    var isFiltering: Bool {
+        return searchController.isActive && !(searchController.searchBar.text?.isEmpty ?? true)
+    }
     
     var toDosSnapshot: NSDiffableDataSourceSnapshot<Section, ToDo.ID> {
         var snapshot = NSDiffableDataSourceSnapshot<Section, ToDo.ID>()
         snapshot.appendSections([.main])
-        snapshot.appendItems(toDos.map { $0.id })
-        return snapshot
-    }
-    
-    var filteredToDosSnapshot: NSDiffableDataSourceSnapshot<Section, ToDo.ID> {
-        var snapshot = NSDiffableDataSourceSnapshot<Section, ToDo.ID>()
-        snapshot.appendSections([.main])
-        snapshot.appendItems(filteredToDos.map { $0.id })
+        snapshot.appendItems(isFiltering ? filteredToDos.map { $0.id } : toDos.map { $0.id })
         return snapshot
     }
 
@@ -56,20 +52,35 @@ class ToDoTableViewController: UITableViewController, UISearchResultsUpdating, T
         return true
     }
 
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            toDos.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .automatic)
-            ToDo.saveToDos(toDos)
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        guard let itemIdentifier = dataSource.itemIdentifier(for: indexPath),
+              let toDoToDelete = toDos.first(where: { $0.id == itemIdentifier }) else {
+            return nil
         }
+
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { [weak self] _, _, completionHandler in
+            guard let self else { return }
+            if let index = toDos.firstIndex(of: toDoToDelete),
+               let filteredIndex = filteredToDos.firstIndex(of: toDoToDelete){
+                toDos.remove(at: index)
+                filteredToDos.remove(at: filteredIndex)
+                dataSource.apply(toDosSnapshot, animatingDifferences: true)
+                ToDo.saveToDos(toDos)
+            }
+            completionHandler(true)
+        }
+
+        return UISwipeActionsConfiguration(actions: [deleteAction])
     }
     
     func checkmarkTapped(sender: ToDoCell) {
-        if let indexPath = tableView.indexPath(for: sender) {
-            var toDo = toDos[indexPath.row]
-            toDo.isComplete.toggle()
-            toDos[indexPath.row] = toDo
-            tableView.reloadRows(at: [indexPath], with: .automatic)
+        if let indexPath = tableView.indexPath(for: sender),
+           let itemIdentifier = dataSource.itemIdentifier(for: indexPath),
+           let index = toDos.firstIndex(where: { $0.id == itemIdentifier }),
+           let filteredIndex = filteredToDos.firstIndex(where: { $0.id == itemIdentifier }){
+            toDos[index].isComplete.toggle()
+            filteredToDos[filteredIndex].isComplete.toggle()
+            dataSource.applySnapshotUsingReloadData(toDosSnapshot)
             ToDo.saveToDos(toDos)
         }
     }
@@ -84,8 +95,7 @@ class ToDoTableViewController: UITableViewController, UISearchResultsUpdating, T
             filteredToDos = toDos
         }
         
-        dataSource.apply(filteredToDosSnapshot, animatingDifferences: true)
-        print(toDos)
+        dataSource.apply(toDosSnapshot, animatingDifferences: true)
     }
     
     func createDataSource() {
@@ -108,13 +118,15 @@ class ToDoTableViewController: UITableViewController, UISearchResultsUpdating, T
         guard segue.identifier == "saveUnwind" else { return }
         let sourceViewController = segue.source as! ToDoDetailTableViewController
         if let toDo = sourceViewController.toDo {
-            if let indexOfExistingToDo = toDos.firstIndex(of: toDo) {
-                toDos[indexOfExistingToDo] = toDo
-                tableView.reloadRows(at: [IndexPath(row: indexOfExistingToDo, section: 0)], with: .automatic)
+            if let index = toDos.firstIndex(of: toDo),
+               let filteredIndex = filteredToDos.firstIndex(of: toDo){
+                toDos[index] = toDo
+                filteredToDos[filteredIndex] = toDo
+                dataSource.applySnapshotUsingReloadData(toDosSnapshot)
             } else {
-                let newIndexPath = IndexPath(row: toDos.count, section: 0)
                 toDos.append(toDo)
-                tableView.insertRows(at: [newIndexPath], with: .automatic)
+                filteredToDos = toDos
+                dataSource.apply(toDosSnapshot, animatingDifferences: true)
             }
         }
         ToDo.saveToDos(toDos)
@@ -129,7 +141,8 @@ class ToDoTableViewController: UITableViewController, UISearchResultsUpdating, T
         
         tableView.deselectRow(at: indexPath, animated: true)
         
-        detailController?.toDo = toDos[indexPath.row]
+        let itemIdentifier = dataSource.itemIdentifier(for: indexPath)
+        detailController?.toDo = toDos.first(where: { $0.id == itemIdentifier })
         
         return detailController
     }
